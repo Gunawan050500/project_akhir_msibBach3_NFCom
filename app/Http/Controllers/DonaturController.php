c34a035d859d44d49ae496e6601fb1a2136bcb31
ahmadsyafii <ahmadsyafii2751@gmail.com>
2022-11-13 12:26:12 +0700

    Ini bagian tabel donatur, tetapi css nya bentrok. Barangkali ada yang bisa memperbaiki atau ada bagian tabel yang lain

---
+++ app/Http/Controllers/DonaturController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Donatur;
+
+class DonaturController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //digunakan untuk menampilkan data secara keseluruhan
+        $donatur = Donatur::all();
+        return view('donatur.index', compact('donatur'));
+        //di compact=> dengan membawa array divisi
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view ('donatur.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|unique:donatur|max:45',
+            'no_hp' => 'required'
+        ]);
+      
+        Donatur::create($request->all());
+       
+        return redirect()->route('donatur.index')
+                        ->with('success','Donatur Berhasil Disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
