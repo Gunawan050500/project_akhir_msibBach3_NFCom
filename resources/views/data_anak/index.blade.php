7327ce43217b66b24e5c2c0e469393b0341f0ba1
ahmadsyafii <ahmadsyafii2751@gmail.com>
2022-12-02 10:59:07 +0700

    Perbaikan tampilan data anak dan data struktur dari Dinda

--- resources/views/data_anak/index.blade.php
+++ resources/views/data_anak/index.blade.php
@@ -1,26 +1,47 @@
 @extends('landingpage.index')
 @section('content')
+<body>
+
+<!-- Spinner Start -->
+<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only"></span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+      <div class="container-xxl py-5">
+        <div class="container">
+    <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h6 class="section-title bg-white text-center px-3">Data</h6>
+                <h1 class="mb-5">Anak Asuh</h1>
+            </div>
 <div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-success">Data Anak Asuh</h6>
-    </div>
-    <div class="row">
-    <div class="col-6">
-    </div>
-        <div class="col-6 my-2 mx-2">
-        <nav class="justify-content-between mr-4" style="width: 40%">
-            <form class="form-inline" action="{{url('data_anak-cari')}}" method="GET">
-                <input class="form-control mr-sm-2 form-sm" type="text" name="cari" placeholder="Search"
-                    aria-label="Search" value="{{ old('cari') }}">
-                <input class="btn btn-outline-success" type="submit" value="Cari">
-            </form>
-        </nav>
-    </div>
-    <div class="card-body">
+<div class="card-body">
+<div class="col-lg-4">
+<div class="mb-5 wow slideInUp" data-wow-delay="0.1s">
+<div class="input-group">
+       <form class="form-inline" action="{{url('data_anak-cari')}}" method="GET">
+       <div class="row">
+       <div class="col-lg-9 col-md-9">
+        <input class="form-control" type="text" name="cari" placeholder="Search"
+        aria-label="Search" value="{{ old('cari') }}">
+        </div>
+
+        <div class="col-lg-3 col-md-3">
+        <button class="btn btn-outline-success px-4"  
+        type="submit"  value="Cari">
+        <i class="bi bi-search"></i></button>
+       </div>
+       </div>
+    </form>
+</div>
+</div>
+</div>
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="70%" cellspacing="0">
                 <thead>
-                    <tr>
+                    <tr bgcolor="#5bc1ac">
                         <th>No</th>
                         <th>Nama</th>
                         <th>Tempat Lahir</th>
@@ -59,4 +80,18 @@
     </div>
 </div>
 </div>
+        </div>
+      </div>
+</body>
+      
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('landingpage/lib/wow/wow.min.js')}}"></script>
+    <script src="{{asset('landingpage/lib/easing/easing.min.js')}}"></script>
+    <script src="{{asset('landingpage/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{asset('landingpage/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{url('landingpage/js2/main.js')}}"></script>
 @endsection
