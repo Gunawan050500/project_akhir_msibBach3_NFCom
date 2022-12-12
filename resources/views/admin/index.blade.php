7066863706534a394aa59b0186b8a71d9c6b63ea
Diindaa <112836825+Diindaa@users.noreply.github.com>
2022-12-12 20:03:36 +0700

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <title>Yayasan Rumah Yatim</title>
-    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    {{--  <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->  --}}
     <!--[if lt IE 11]>
     	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -17,15 +17,28 @@
     <meta name="keywords" content="">
     <meta name="author" content="Phoenixcoded" />
     <!-- Favicon icon -->
-    <link rel="icon" href="{{ url ('admin/images/favicon.ico') }}" type="image/x-icon">
+    <!--link rel="icon" href="{{ url ('admin/images/favicon.ico') }}" type="image/x-icon"-->
+    <link href="{{url('landingpage/images/logo.png')}}" rel="shortcut icon">
 
-    <!-- vendor css -->
-    <link rel="stylesheet" href="{{ asset ('admin/css/style.css') }}">
-    
-    
+    {{--  link bootstrap icons  --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.1/font/bootstrap-icons.css">
 
+    {{--  link bootstrap
+    <!-- CSS only -->  --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    {{--  link js bootstrap  --}}
+    {{--  <!-- JavaScript Bundle with Popper -->  --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    {{--  <!-- vendor css -->  --}}
+    <link rel="stylesheet" href="{{ asset ('admin/css/style.css') }}"> 
+
+    {{--  MDB  --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"
+    rel="stylesheet"/>
+    @livewireStyles
 </head>
 <body class="">
+    @include('sweetalert::alert')
 	<!-- [ Pre-loader ] start -->
 	<div class="loader-bg">
 		<div class="loader-track">
@@ -46,7 +59,7 @@
 <div class="pcoded-main-container">
     <div class="pcoded-content">
         <!-- [ breadcrumb ] start -->
-        <div class="page-header">
+        <!--<div-- class="page-header">
             <div class="page-block">
                 <div class="row align-items-center">
                     <div class="col-md-12">
@@ -60,7 +73,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div-->
         <!-- [ breadcrumb ] end -->
         <!-- [ Main Content ] start -->
         <div class="row">
@@ -70,11 +83,15 @@
     </div>
 </div>
 
-
     <!-- Required Js -->
     <script src="{{ asset ('admin/js/vendor-all.min.js') }}"></script>
     <script src="{{ asset ('admin/js/plugins/bootstrap.min.js') }}"></script>
     <script src="{{ asset ('admin/js/pcoded.min.js') }}"></script>
+
+    <!-- MDB -->
+    <script
+    type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"></script>
 
 <!-- Apex Chart -->
 <script src="{{ asset ('admin/js/plugins/apexcharts.min.js') }}"></script>
@@ -82,6 +99,7 @@
 
 <!-- custom-chart js -->
 <script src="{{ asset ('admin/js/pages/dashboard-main.js') }}"></script>
+@livewireScripts
 </body>
 
 </html>
